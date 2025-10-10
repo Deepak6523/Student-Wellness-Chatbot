140e413b10eaee11c5838fa90ee2dc2c6177706e
deep123 <162825065+Deepak6523@users.noreply.github.com>
2025-10-11 00:20:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ st.set_page_config(page_title="Student Wellness Chatbot", page_icon="🌱", layo
 # -------------------------------
 # 🔑 2. Gemini API Setup
 # -------------------------------
-GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+GEMINI_API_KEY = st.secrets["GEMINI"]["GEMINI_API_KEY"]
+
 API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
 
 # -------------------------------
